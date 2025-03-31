8fba67758e5a581edfe35ec3f0cd0103ea93e4dc
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-03-31 19:09:58 -0300

    Servidor finalizado e adicionado ao globals

--- network/server/server.gd
+++ network/server/server.gd
@@ -9,30 +9,55 @@ var _enet: ENetMultiplayerPeer
 
 
 func start_server() -> void:
-	_setup_enet()
+	var error: Error = _setup_enet()
+	if error != Error.OK:
+		return
 
 	_enet.peer_connected.connect(_peer_connected)
 	_enet.peer_disconnected.connect(_peer_disconnected)
 
 
-func _setup_enet() -> void:
+func _setup_enet() -> Error:
 	_enet = ENetMultiplayerPeer.new()
-	var error = _enet.create_server(
+	var error: Error = _enet.create_server(
 		Constants.connection_port,
 		Constants.max_connections
 	)
 
-	if error != OK:
-		print("Erro ao iniciar o servidor ENet:", error)
-		return
+	if error != Error.OK:
+		printerr("Erro ao iniciar o servidor, código do erro:", error)
+		_enet = null
+		return error
 
+	print("Servidor iniciado com sucesso na porta:", Constants.connection_port)
 	multiplayer.multiplayer_peer = _enet
-	print("Servidor iniciado com sucesso na porta: ", Constants.connection_port)
+
+	return Error.OK
 
 
 func _peer_connected(pid: int) -> void:
-	pass
+	var peer: ENetPacketPeer = _enet.get_peer(pid)
+	if not peer:
+		return
+
+	Globals.connections[pid] = {
+		"id": pid,
+		"peer": peer
+	}
+
+	print("Nova conexão ao servidor com o ip: ", peer.get_remote_address())
+	peer_connected.emit(pid)
 
 
 func _peer_disconnected(pid: int) -> void:
-	pass
+	if not Globals.connections.has(pid):
+		printerr("Tentativa de desconexão de um ID inexistente:", pid)
+		return
+
+	var connection: Dictionary = Globals.connections[pid]
+	var connection_peer: ENetPacketPeer = connection.get("peer", null)
+
+	Globals.connections.erase(pid)
+
+	print("Conexão com o ip: ", connection_peer.get_remote_address(), ", deixando o servidor!")
+	peer_disconnected.emit(pid)

3f60c1a7f5caa6f0e1a04a6d3373cc090172815a
matheusoreis <matheuss_reis@outlook.com>
2025-09-18 15:00:19 -0300

    Correções do novo diretório

--- client/system/utils/alert.gd
+++ client/system/utils/alert.gd
@@ -5,7 +5,7 @@ var alert: AlertUi
 
 
 func show(message: String) -> void:
-	var alert_scene: PackedScene = load("res://database/ui/shared/alert.tscn")
+	var alert_scene: PackedScene = load("res://database/interfaces/shared/alert.tscn")
 	alert = alert_scene.instantiate()
 	alert.add_message(message)
 
